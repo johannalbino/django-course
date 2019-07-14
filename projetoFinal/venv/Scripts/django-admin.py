5336f184bd33b919281ba8bf0f38adf0a67cfbfa
Johann Albino <johann.albino.ti@gmail.com>
2019-07-13 22:48:21 -0300

    Course Django - Projetos

---
+++ projetoFinal/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\johann herbert\desktop\django-course\projetofinal\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
